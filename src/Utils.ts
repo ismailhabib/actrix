a8fd43ccb4a334d1ea86b231439e1303435ddf1c
Habib <ismail.muhammad@mendix.com>
2018-03-29 14:13:48 +0200

    Change some configurations

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,121 +1,126 @@
-export type CancellablePromise<T> = Promise<T> & { cancel(): void }
+export type CancellablePromise<T> = Promise<T> & { cancel(): void };
 
 export type Promisify<
-  T extends (...args: any[]) => IterableIterator<any>
+    T extends (...args: any[]) => IterableIterator<any>
 > = T extends () => IterableIterator<infer B1>
-  ? Exclude<B1, Promise<any>> extends never
-    ? () => CancellablePromise<void>
-    : () => CancellablePromise<Exclude<B1, Promise<any>>>
-  : T extends (a1: infer A1) => IterableIterator<infer B1>
     ? Exclude<B1, Promise<any>> extends never
-      ? (a1: A1) => CancellablePromise<void>
-      : (a1: A1) => CancellablePromise<Exclude<B1, Promise<any>>>
-    : T extends (a1: infer A1, a2: infer A2) => IterableIterator<infer B1>
-      ? Exclude<B1, Promise<any>> extends never
-        ? (a1: A1, a2: A2) => CancellablePromise<void>
-        : (a1: A1, a2: A2) => CancellablePromise<Exclude<B1, Promise<any>>>
-      : T extends (a1: infer A1, a2: infer A2, a3: infer A3) => IterableIterator<infer B1>
+        ? () => CancellablePromise<void>
+        : () => CancellablePromise<Exclude<B1, Promise<any>>>
+    : T extends (a1: infer A1) => IterableIterator<infer B1>
         ? Exclude<B1, Promise<any>> extends never
-          ? (a1: A1, a2: A2, a3: A3) => CancellablePromise<void>
-          : (a1: A1, a2: A2, a3: A3) => CancellablePromise<Exclude<B1, Promise<any>>>
-        : T extends (
-            a1: infer A1,
-            a2: infer A2,
-            a3: infer A3,
-            a4: infer A4
-          ) => IterableIterator<infer B1>
-          ? Exclude<B1, Promise<any>> extends never
-            ? (a1: A1, a2: A2, a3: A3, a4: A4) => CancellablePromise<void>
-            : (a1: A1, a2: A2, a3: A3, a4: A4) => CancellablePromise<Exclude<B1, Promise<any>>>
-          : T extends (
-              a1: infer A1,
-              a2: infer A2,
-              a3: infer A3,
-              a4: infer A4,
-              a5: infer A5
-            ) => IterableIterator<infer B1>
+            ? (a1: A1) => CancellablePromise<void>
+            : (a1: A1) => CancellablePromise<Exclude<B1, Promise<any>>>
+        : T extends (a1: infer A1, a2: infer A2) => IterableIterator<infer B1>
             ? Exclude<B1, Promise<any>> extends never
-              ? (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5) => CancellablePromise<void>
-              : (
-                  a1: A1,
-                  a2: A2,
-                  a3: A3,
-                  a4: A4,
-                  a5: A5
-                ) => CancellablePromise<Exclude<B1, Promise<any>>>
-            : never
+                ? (a1: A1, a2: A2) => CancellablePromise<void>
+                : (a1: A1, a2: A2) => CancellablePromise<Exclude<B1, Promise<any>>>
+            : T extends (a1: infer A1, a2: infer A2, a3: infer A3) => IterableIterator<infer B1>
+                ? Exclude<B1, Promise<any>> extends never
+                    ? (a1: A1, a2: A2, a3: A3) => CancellablePromise<void>
+                    : (a1: A1, a2: A2, a3: A3) => CancellablePromise<Exclude<B1, Promise<any>>>
+                : T extends (
+                      a1: infer A1,
+                      a2: infer A2,
+                      a3: infer A3,
+                      a4: infer A4
+                  ) => IterableIterator<infer B1>
+                    ? Exclude<B1, Promise<any>> extends never
+                        ? (a1: A1, a2: A2, a3: A3, a4: A4) => CancellablePromise<void>
+                        : (
+                              a1: A1,
+                              a2: A2,
+                              a3: A3,
+                              a4: A4
+                          ) => CancellablePromise<Exclude<B1, Promise<any>>>
+                    : T extends (
+                          a1: infer A1,
+                          a2: infer A2,
+                          a3: infer A3,
+                          a4: infer A4,
+                          a5: infer A5
+                      ) => IterableIterator<infer B1>
+                        ? Exclude<B1, Promise<any>> extends never
+                            ? (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5) => CancellablePromise<void>
+                            : (
+                                  a1: A1,
+                                  a2: A2,
+                                  a3: A3,
+                                  a4: A4,
+                                  a5: A5
+                              ) => CancellablePromise<Exclude<B1, Promise<any>>>
+                        : never;
 export function promisify<T extends (...args: any[]) => IterableIterator<any>>(
-  generator: T
+    generator: T
 ): Promisify<T> {
-  // Implementation based on https://github.com/tj/co/blob/master/index.js
-  return function() {
-    const args = arguments
-    const ctx = this
-    // const runId = ++generatorId;
-    const gen: IterableIterator<any> = generator.apply(ctx, args)
-    let rejector: (error: any) => void
-    let pendingPromise: CancellablePromise<any> | undefined = undefined
+    // Implementation based on https://github.com/tj/co/blob/master/index.js
+    return function() {
+        const args = arguments;
+        const ctx = this;
+        // const runId = ++generatorId;
+        const gen: IterableIterator<any> = generator.apply(ctx, args);
+        let rejector: (error: any) => void;
+        let currentPromise: CancellablePromise<any> | undefined = undefined;
 
-    const res = new Promise(function(resolve, reject) {
-      let stepId = 0
-      rejector = reject
+        const res = new Promise(function(resolve, reject) {
+            let stepId = 0;
+            rejector = reject;
 
-      function onFulfilled(res: any) {
-        pendingPromise = undefined
-        let ret
-        try {
-          ret = gen.next(res)
-        } catch (e) {
-          return reject(e)
-        }
+            function onFulfilled(res: any) {
+                currentPromise = undefined;
+                let ret;
+                try {
+                    ret = gen.next(res);
+                } catch (e) {
+                    return reject(e);
+                }
 
-        next(ret)
-      }
+                next(ret);
+            }
 
-      function onRejected(err: any) {
-        pendingPromise = undefined
-        let ret
-        try {
-          ret = gen.throw!(err)
-        } catch (e) {
-          return reject(e)
-        }
-        next(ret)
-      }
+            function onRejected(err: any) {
+                currentPromise = undefined;
+                let ret;
+                try {
+                    ret = gen.throw!(err);
+                } catch (e) {
+                    return reject(e);
+                }
+                next(ret);
+            }
 
-      function next(ret: any) {
-        if (ret && typeof ret.then === 'function') {
-          // an async iterator (you mean a promise?)
-          ret.then(next, reject)
-          return
-        }
-        if (ret.done) {
-          return resolve(ret.value)
-        }
-        pendingPromise = Promise.resolve(ret.value) as any
-        return pendingPromise!.then(onFulfilled, onRejected)
-      }
+            function next(ret: any) {
+                if (ret && typeof ret.then === 'function') {
+                    // an async iterator (you mean a promise?)
+                    ret.then(next, reject);
+                    return;
+                }
+                if (ret.done) {
+                    return resolve(ret.value);
+                }
+                currentPromise = Promise.resolve(ret.value) as any;
+                return currentPromise!.then(onFulfilled, onRejected);
+            }
 
-      onFulfilled(undefined) // kick off the process
-    }) as any
+            onFulfilled(undefined); // kick off the process
+        }) as any;
 
-    res.cancel = function() {
-      try {
-        if (pendingPromise && typeof pendingPromise.cancel === 'function') {
-          pendingPromise.cancel()
-        }
-        gen.return!()
-        rejector(new Error('PROMISE_CANCELLED'))
-      } catch (e) {
-        rejector(e) // there could be a throwing finally block
-      }
-    }
-    return res
-  } as Promisify<T>
+        res.cancel = function() {
+            try {
+                if (currentPromise && typeof currentPromise.cancel === 'function') {
+                    currentPromise.cancel();
+                }
+                gen.return!();
+                rejector(new Error('PROMISE_CANCELLED'));
+            } catch (e) {
+                rejector(e); // there could be a throwing finally block
+            }
+        };
+        return res;
+    } as Promisify<T>;
 }
 
 function cancelPromise(promise: any) {
-  if (typeof promise.cancel === 'function') {
-    promise.cancel()
-  }
+    if (typeof promise.cancel === 'function') {
+        promise.cancel();
+    }
 }
