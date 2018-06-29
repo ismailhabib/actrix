7e001b67a2a226a0a27dc48021e9ed90b3a40cc4
Habib <habsky@gmail.com>
2018-06-29 15:53:58 +0200

    feat: Make the new test utility available

--- src/actrix.ts
+++ src/actrix.ts
@@ -1,7 +1,8 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 // ...
-export * from './Actor'
-export * from './ActorSystem'
-export * from './interfaces'
-export * from './Utils'
+export * from "./Actor";
+export * from "./ActorSystem";
+export * from "./interfaces";
+export * from "./Utils";
+export * from "./TestUtils";
